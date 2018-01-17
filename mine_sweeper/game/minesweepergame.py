0b34c5eed3aa3fa600e34ee6f5560210874d8ed3
johncoleman83 <djc_fileno@davidjohncoleman.com>
2018-01-16 16:31:28 -0800

    created initial game loop with basic edge cases

--- mine_sweeper/game/minesweepergame.py
+++ mine_sweeper/game/minesweepergame.py
@@ -1,29 +1,55 @@
 #!/usr/bin/env python3
 from datetime import datetime
 import random
+import re
 import sys
 
 class MineSweeperGame():
     """
     Minesweeper Game
     """
-    def __init__(self, height, width, mines):
+    def __init__(self, *args):
         """
         initializes MineSweeper game solver
         """
-        self.init_vals_and_verify_game_inputs(height, width, mines)
-        self.B = {}
-        self.revealed = set()
-        self.initialize_board()
+        self.init_vals_and_verify_game_inputs(args[0])
 
-    def init_vals_and_verify_game_inputs(self, h, w, m):
-        if 5 > h > 24 or 5 > w > 30 or m > h * w:
+    def tokenize(self, s):
+        if s == "":
+            return []
+        regex = re.compile("([0-9]*)")
+        tokens = regex.findall(s)
+        return [s for s in tokens if not s.isspace() and s != '']
+
+    def tokenize_input(self, choice):
+        tokens = self.tokenize(choice)
+        if len(tokens) == 2:
+            return int(tokens[0]), int(tokens[1])
+        else:
+            return None, None
+
+    def tokenize_args(self, s):
+        tokens = self.tokenize(s)
+        if len(tokens) == 3:
+            return int(tokens[0]), int(tokens[1]), int(tokens[2])
+        else:
+            return None, None, None
+
+    def init_vals_and_verify_game_inputs(self, args):
+        w, h, m = self.tokenize_args(args)
+        if w is None or h is None or m is None or 5 > h > 24 or 5 > w > 30 or m > h * w:
             raise Exception("invalid game coordinates")
         self.R = h
         self.C = w
         self.M = m
+        self.B = {}
+        self.revealed = set()
+        self.initialize_board()
+        self.finished = False
+        if self.more_unrevealed_spaces() is False:
+            raise Exception("this board is invalid due to already having been completed")
 
-    def display_game(self):
+    def display_game(self, reveal=False):
         print("   ", end="")
         for x in range(self.C - 1):
             print(x, end=" ")
@@ -36,10 +62,15 @@ class MineSweeperGame():
             print(r, end="| ")
             for c in range(self.C - 1):
                 piece = self.B.get((r, c))
-                # print(piece, end=" ")
-                print(piece if piece in self.revealed else '?', end=" ")
+                if reveal is True:
+                    print(piece, end=" ")
+                else:
+                    print(piece if (r, c) in self.revealed else '?', end=" ")
             piece = self.B.get((r, self.C - 1))
-            print(piece if piece in self.revealed else '?')
+            if reveal is True:
+                print(piece)
+            else:
+                print(piece if (r, self.C - 1) in self.revealed else '?')
 
     def set_space_values(self):
         for r in range(self.R):
@@ -73,10 +104,47 @@ class MineSweeperGame():
         self.place_mines()
         self.set_space_values()
 
+    def more_unrevealed_spaces(self):
+        if len(self.revealed) + self.M < self.R * self.C:
+            return True
+        return False
+
+    def game_loop(self):
+        while self.finished == False:
+            self.display_game()
+            print("Your move must be an input of 2 space separated integers"
+                  " e.g. row column: '5 5'")
+            choice = input("what position would you like to reveal?  ")
+            if choice == "quit":
+                break
+            r, c = self.tokenize_input(choice)
+            if r is None or c is None:
+                print("invalid input, please try again")
+                continue
+            space = self.B.get((r, c), None)
+            if space is None:
+                print("invalid board space, please try again")
+            elif space == '*':
+                print()
+                print("BOOOOM!!")
+                print()
+                self.display_game(reveal=True)
+                self.finished = True
+            elif (r, c) in self.revealed:
+                print("space is already revealed, please try again")
+            else:
+                self.revealed.add((r, c))
+                if self.more_unrevealed_spaces() is False:
+                    self.finished = True
+                    print("congratulations!!")
+                    self.display_game(reveal=True)
 
 def main_app():
-    game = MineSweeperGame(10, 10, 10)
-    game.display_game()
+    print("What board dimensions would you like?")
+    print("input 3 space separated values: board width, height, and mine count")
+    choice = input("e.g. 10 10 10   ")
+    game = MineSweeperGame(choice)
+    game.game_loop()
 
 if __name__ == "__main__":
     """
